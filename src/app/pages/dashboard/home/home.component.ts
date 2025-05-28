31a87e6f5121f191a58b07983eae86cce1453571
Carlos Morales <--help>
2025-05-28 10:49:50 -0600

    Imports Fixed

--- src/app/pages/dashboard/home/home.component.ts
+++ src/app/pages/dashboard/home/home.component.ts
@@ -2,13 +2,11 @@ import { Component } from '@angular/core';
 import { DashboardSectionComponent } from '../../../shared/components/dashboard-section/dashboard-section.component';
 import { OwlCardsComponent } from './owl-cards/owl-cards.component';
 import { ToolCardsComponent } from '../../../shared/components/tool-cards/tool-cards.component';
-import { ModalComponent } from '../../../shared/components/modal/modal.component';
 @Component({
   selector: 'app-home',
   imports: [DashboardSectionComponent,
     OwlCardsComponent,
     ToolCardsComponent,
-    ModalComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
